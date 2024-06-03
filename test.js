669c0de27d1297d28aac00b1c3b300e0c4faa00e
Valery Cordero <vc107847@harriscomputer.com>
2024-06-02 21:05:13 -0600

    Branch Second Modified file to the project

--- test.js
+++ test.js
@@ -4,10 +4,7 @@ document.querySelector('#user-name').value = 'problem_user'
 document.querySelector('#password').value = 'secret_sauce'
 document.querySelector('#login-button ').click()
 
-//edition test 3
+//edition test 
 
 document.querySelector('#item_4_title_link > div').innerText();
 
-//edition test 
-
-//edition test 2
