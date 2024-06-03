1911dc2605c3e70e8ff711acf55092b1d93a60b0
Valery Cordero <vc107847@harriscomputer.com>
2024-06-02 20:57:55 -0600

    Modified file to the project

--- test.js
+++ test.js
@@ -2,6 +2,8 @@
 
 document.querySelector('#user-name').value = 'problem_user'
 document.querySelector('#password').value = 'secret_sauce'
-document.querySelector('#login-button').click()
+document.querySelector('#login-button ').click()
 
 document.querySelector('#item_4_title_link > div').innerText();
+
+//edition test 
